5dadd9777e2e831c7c4a7411cf10530604ce19fb
gustavoventieri <ventierigustavo@gmail.com>
2025-02-26 20:18:49 -0300

    feat: lesson 4 added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,42 @@
 package org.example;
 
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-    MinhaSegundaED ED = new MinhaSegundaED();
+        ListaEncadeada lista = new ListaEncadeada();
 
+        // Criando objetos Pessoa
+        Pessoa p1 = new Pessoa(1, "João", "joao@gmail.com");
+        Pessoa p2 = new Pessoa(2, "Maria", "maria@gmail.com");
+        Pessoa p3 = new Pessoa(3, "Carlos", "carlos@gmail.com");
+
+        // Testando adicionar elementos
+        lista.adicionar(p1);
+        lista.adicionar(p2);
+        lista.adicionar(p3);
+
+        // Exibindo os elementos
+        System.out.println("Lista após adição:");
+        exibirLista(lista);
+
+        // Testando remoção
+        lista.remover();
+        System.out.println("Lista após remover último elemento:");
+        exibirLista(lista);
+
+        // Removendo até esvaziar
+        lista.remover();
+        lista.remover();
+        System.out.println("Lista após remover todos:");
+        exibirLista(lista);
+
+        // Tentando remover de lista vazia
+        lista.remover();
+    }
+
+    private static void exibirLista(ListaEncadeada lista) {
+        while (lista.temProximo()) {
+            System.out.println(lista.getPosicaoAtual().getValor());
+        }
     }
 }
